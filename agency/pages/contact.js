f6c4a2c98bfdf471ac9c86d234ce9a59be8fd2dd
jibujacob <jibupjacob@gmail.com>
2021-12-19 15:00:42 +0530

    Contact Page Completed

---
+++ agency/pages/contact.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Circle from '../components/Circle'
+import styles from '../styles/Contact.module.css'
+
+const contact = () => {
+    return (
+        <div className={styles.container}>
+            <Circle backgroundColor="green" left="-40vh" top="-20vh"/>
+            <Circle backgroundColor="yellow" right="-30vh" bottom="-60vh"/>
+            <h1 className={styles.title}>GET IN TOUCH</h1>
+            <form className={styles.form}>
+                <input type="text" className={styles.inputS} placeholder="Username"/>
+                <input type="text" className={styles.inputS} placeholder="Phone"/>
+                <input type="text" className={styles.inputL} placeholder="Email"/>
+                <input type="text" className={styles.inputL} placeholder="Subject"/>
+                <textarea className={styles.textarea} 
+                    placeholder="Message"
+                    rows={6}
+                    />
+                <button className={styles.button}>SUBMIT</button>
+            </form>
+        </div>
+    )
+}
+ 
+export default contact
