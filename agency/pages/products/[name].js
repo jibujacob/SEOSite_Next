74670fc2c175c061c3ed08bddf30ab201441e5e4
jibujacob <jibupjacob@gmail.com>
2021-12-19 14:33:49 +0530

    Product dynamic pages Completed

---
+++ agency/pages/products/[name].js
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { data } from '../../data'
+import styles from  '../../styles/Product.module.css'
+
+const Product = ({product}) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.cardL}>
+                {product.images.map(image => {
+                    return (
+                        <div key={image.id} className={styles.imgContainer}>
+                            <Image src={image.url}
+                                alt=""
+                                width="100%"
+                                height="100%"
+                                objectFit="cover"
+                                layout="responsive"
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+            <div className={styles.cardS}>
+                <h1 className={styles.title}>{product.title}</h1>
+                <p className={styles.desc}>{product.longDesc}</p>
+                <button className={styles.button}>
+                    <Link href="/contact">Contact</Link>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export const getStaticPaths = async () => {
+    const products = data;
+    const paths = products.map((item) => {
+      return {
+        params: { name: item.name },
+      };
+    });
+    return {
+      paths,
+      fallback: false,
+    };
+  };
+
+export const getStaticProps = async(ctx) => {
+    const name = ctx.params.name
+    const product = data.filter(item => item.name === name)[0]
+    return {
+        props:{product}
+    }
+}
+
+export default Product
