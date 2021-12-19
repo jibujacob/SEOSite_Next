854b58138f541d43617e6fc9e0994d5e46917ff5
jibujacob <jibupjacob@gmail.com>
2021-12-19 13:17:35 +0530

    Footer Component Completed

--- agency/components/Footer.jsx
+++ agency/components/Footer.jsx
@@ -1,9 +1,43 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+import styles from "../styles/Footer.module.css"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.carL}>
+                <h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
+                <h1 className={styles.linkTitle}>
+                    <a href="/contact" passHref className={styles.link}>
+                        
+                            <span className={styles.linkText}>WORK WITH US</span>
+                            <Image src="/img/link.png"
+                                alt="link.png"
+                                width="40px"
+                                height="40px"
+                            />
+                        
+                    </a>
+                </h1>
+            </div>
+            <div className={styles.carS}>
+                <div className={styles.cardItem}>
+                    159 DILSHAD COLONY DELHI <br/> INDIA
+                </div>
+                <div className={styles.cardItem}>
+                    CONTACT@JJ.DEV <br/> 91 12345 67890
+                </div>
+            </div>
+            <div className={styles.carS}>
+                <div className={styles.cardItem}>
+                    FOLLOW US: <br/> __FB __IN __BE __TW
+                </div>
+                <div className={styles.cardItem}>
+                    &copy; 2022 JJ INTERACTIVE, <br/> ALL RIGHTS RESERVED
+                </div>
+            </div>
         </div>
     )
 }
